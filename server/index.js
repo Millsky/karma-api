d47377933f30af829c0b9092c5315aeca2b1f14e
millsky <kyle83567@gmail.com>
2020-06-07 17:20:11 -0400

    Feature: (Security)
    1. Add error messages / return 500s
    2. Add in helmet for header security

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+const helmet = require('koa-helmet');
 const Koa = require('koa');
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
@@ -23,6 +24,7 @@ router
         ctx.body = res.rows[0];
       } catch (e) {
         console.log(e);
+        ctx.response.status = 500;
       }
       ctx.body = ctx.body || null;
   })
@@ -34,6 +36,7 @@ router
         ctx.body = res.rows[0];
       } catch (e) {
         console.log(e);
+        ctx.response.status = 500;
       }
       ctx.body = ctx.body || null;
   })
@@ -45,43 +48,49 @@ router
       ctx.body = res.rows;
     } catch (e) {
       console.log(e);
+      ctx.response.status = 500;
     }
-    ctx.body = ctx.body || null;
   })
   .post('karma', async (ctx) => {
     const { from_user_id, message, to_user_id, group_id } = ctx.request.body || {};
     let karma_id = null;
+    console.log('Adding to a users karma');
     try {
       const res = await pool.query(`SELECT * FROM insert_karma($1, $2, $3, $4)`, [from_user_id, to_user_id, group_id, message]);
       karma_id = res.rows[0].insert_karma;
+      ctx.body = {
+        from_user_id,
+        message,
+        group_id,
+        to_user_id,
+        karma_id,
+      }
     } catch (e) {
       console.log(e);
-    }
-    ctx.body = {
-      from_user_id,
-      message,
-      group_id,
-      to_user_id,
-      karma_id,
+      ctx.response.status = 500;
     }
   })
   .post('group', async (ctx) => {
     const { name } = ctx.request.body || {};
+    console.log('Adding a new group');
     try {
         const res = await pool.query(`SELECT * FROM insert_group($1)`, [name]);
         ctx.body = res.rows[0].insert_group;
     } catch (e) {
       console.log(e);
+      ctx.response.status = 500;
     }
-    ctx.body = ctx.body || null;
+
   })
   .get('group/:id', async (ctx) => {
     const { id } = ctx.params;
+    console.log('Getting a group');
     try {
       const res = await pool.query('SELECT * FROM get_group($1)', [id]);
       ctx.body = res.rows[0];
     } catch (e) {
       console.log(e);
+      ctx.response.status = 500;
     }
     ctx.body = ctx.body || null;
   });
@@ -89,4 +98,5 @@ router
 app.use(bodyParser());
 app.use(router.routes());
 app.use(router.allowedMethods());
+app.use(helmet());
 app.listen(8080);
